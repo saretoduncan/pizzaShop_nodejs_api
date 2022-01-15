f82ec567b4f9050e1f686ec966b18197c9931b99
saretoduncan <saretoduncan@gmail.com>
2022-01-15 09:13:40 +0300

    install and use bycryp for password hashing

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,21 +1,27 @@
 import express from "express";
 import { User } from "../models/Useschema.js";
 import { signUpValidation, validate,loginValidation } from "../middleWare/validator.js";
+import bcrypt from 'bcrypt'
 
 const routes = express.Router();
 //register users
 routes.post("/register", signUpValidation(), validate, async (req, res) => {
   const { name, email, password } = req.body;
-  const user = new User({
-    name: name,
-    email: email,
-    password: password,
-  });
+ 
+  //validate if email exists
  const emailExitst= await User.findOne({email:email})
  if(emailExitst) return res.status(400).json("email already exists");
- 
+ //hash password
+ const salt = await bcrypt.genSalt(10);
+ const hashPassword = await bcrypt.hash(password, salt);
+ //create new user from the schema
+ const user = new User({
+  name: name,
+  email: email,
+  password: hashPassword,
+});
   try {
-    const savedUser = await user.save();
+    const savedUser = await user.save();//save user to db
     res.json(savedUser);
   } catch (err) {
     res.json(err.message);
