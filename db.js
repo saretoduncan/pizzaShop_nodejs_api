0361f11789289e08998b30b3191cd73b882b64df
saretoduncan <saretoduncan@gmail.com>
2022-01-19 07:42:14 +0300

    change mongodb from local to atlas

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-const mongoDb = mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/pizzastore",
-  () => {
-    console.log(`db connected successfully`);
-  },
-);
+const mongoDb = mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log(`db connected successfully`);
+});
 export default mongoDb;
