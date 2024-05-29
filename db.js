bdb932387dea26919b6afa148b7ccfdeb7e68aa9
Duncan Moiyo <duncan.moiyo@gmail.com>
2024-05-29 11:44:21 +0300

    update mongo db configuration

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-const mongoDb =  mongoose.connect (process.env.MONGODB_URI, () => {
+mongoose.set("strictQuery", true);
+const mongoDb = mongoose.connect(process.env.MONGODB_URI, () => {
   console.log(`db connected successfully`);
 });
 export default mongoDb;
